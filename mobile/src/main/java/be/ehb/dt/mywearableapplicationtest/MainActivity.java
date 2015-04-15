7383ae7db417272ee18fd35c8defaf87e9af69a2
Bert Van Rillaer <vanrillaer@gmail.com>
2015-04-15 11:45:15 +0200

    Initial commit

---
+++ mobile/src/main/java/be/ehb/dt/mywearableapplicationtest/MainActivity.java
@@ -0,0 +1,66 @@
+package be.ehb.dt.mywearableapplicationtest;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonNotificationClicked(View v){
+        int notificationId = 001;
+    // Build intent for notification content
+        //Intent viewIntent = new Intent(this, ViewEventActivity.class);
+        //viewIntent.putExtra(EXTRA_EVENT_ID, eventId);
+        //PendingIntent viewPendingIntent =
+          //      PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.common_signin_btn_icon_dark)
+                        .setContentTitle("Hi Bert")
+                        .setContentText("HI this is the content text")
+                        ;
+
+// Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+// Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+}
